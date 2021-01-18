6f056672c658dea0131327da7e2ebd0414471117
이기준 <2stndard@gmail.com>
2021-01-18 22:03:20 +0900

    5

--- ability.R
+++ ability.R
@@ -14,7 +14,7 @@ inji %>% ggplot(aes(x = fct_reorder(div, value), y = value)) +
   geom_point(aes(color = year, shape = year), size = 3) + 
   coord_flip() + 
   geom_path(arrow = arrow(type = 'closed', angle = 30, length = unit(0.1, "inches")), color = 'black') +
-  geom_text(aes(x = div, y = value, label = value), vjust = 1.5) + 
+  geom_text(aes(x = div, y = value, label = paste0(value, '%')), vjust = 1.5) + 
   labs(y = '인지적 성과의 \'향상됨\'이상 응답 비율(일반대)',
        x = NULL, 
        caption = '출처 : 남신동, 대학의 교수·학습 질 제고 전략 탐색 연구(VII), 한국교육개발원, 2019') +
